e48fbbf9f706922973dafb4f2dfcdd446b5508f5
Rudnik <k.rudnyk@1plus1.tv>
2022-07-01 23:50:48 +0300

    Added widgets controller and Recommended widget

---
+++ src/Controller/EmbedController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EmbedController extends AbstractController
+{
+    public function showSimilarProducts(ProductRepository $productRepository, int $productCount = 2, int $categoryId = null): Response
+    {
+        $params = [];
+        if ($categoryId) {
+            $params['category'] = $categoryId;
+        }
+
+        $products = $productRepository->findBy($params, ['id' => 'DESC'], $productCount);
+        return $this->render('main/_embed/_similar_product.html.twig', [
+            'products' => $products,
+        ]);
+    }
+}
