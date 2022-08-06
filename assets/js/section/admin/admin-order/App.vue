294a853c282a225f205c4b783265ff551c677bbd
Rudnik <k.rudnyk@1plus1.tv>
2022-08-06 15:28:40 +0300

    Implemented Vue

---
+++ assets/js/section/admin/admin-order/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Vue app: {{ productsCount }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  computed: {
+     productsCount: () => {
+       return 123;
+     }
+  }
+}
+</script>
